88acd0af965eef11a6f508bcf60c0c8295f1d641
Arnav Singhvi <arnav11.singhvi@gmail.com>
2023-04-20 08:23:35 -0700

    testing graphs2

--- poet/graph_script.py
+++ poet/graph_script.py
@@ -1,22 +1,21 @@
 import subprocess
 
-# Run the command and capture the output
-models = ['resnet18', "resnet18_patch", "resnet18_fused", "resnet18_inplace","resnet18_pretrained"]
+models = ['resnet18', 'resnet18_patch', 'resnet18_fused', 'resnet18_inplace', 'resnet18_pretrained']
 
-import subprocess
-import sys
+for model in models:
+    output_file = f'{model}_output.txt'
+    with open(output_file, 'w') as f:
+        cmd = ['python', 'poet/solve.py', '--model', model, '--platform', 'a72', '--ram-budget', '3000000', '--runtime-budget', '7.6']
+        try:
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            while True:
+                output = p.stdout.readline()
+                if not output:
+                    break
+                print(output.decode().strip())
+                f.write(output.decode())
 
-with open("output.txt", "w") as f:
-    for m in models:
-        cmd = ["python", "poet/solve.py", "--model", m, "--platform", "a72", "--ram-budget", "3000000", "--runtime-budget", "7.6"]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    while True:
-        output = p.stdout.readline()
-        if not output:
-            break
-        print(output.decode(sys.stdout.encoding), end="")
-        f.write(output.decode(sys.stdout.encoding))
-
-    p.wait()
-
+            p.wait()
+        except subprocess.CalledProcessError as e:
+            print(f'Error running command: {e}')
+            continue
